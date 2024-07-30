7a3364eb2719d6015252a266a15f137d96c0c50e
Narongrit3125 <nbplaying4343@gmail.com>
2024-07-30 09:42:31 +0700

    Second commit

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,12 @@
 let express = require('express')
 let bodyParser = require('body-parser')
 
+const { sequelize } = require('./models')
+
+const config = require('./config/config')
+
 const app = express()
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -19,8 +24,10 @@ app.get('/hello/:person', function (req, res) {
 app.post('/hello', function (req, res) {
     res.send('OK you post - ' + req.body.name)
 })
+let port = process.env.PORT || config.port
 
-let port = 8081
-app.listen(port, function () {
-    console.log('server running on ' + port)
+sequelize.sync({ force: false }).then(() => {
+    app.listen(port, function () {
+        console.log('Server running on ' + port)
+    })
 })
--- server/src/models/index.js
+++ server/src/models/index.js
@@ -15,7 +15,7 @@ fs.readdirSync(__dirname)
         file !== 'index.js'
     ).
     forEach((file) => {
-        const model = sequelize.import(path.join(__dirname, file))
+        const model = require(path.join(__dirname, file))(sequelize , Sequelize.DataTypes);
         db[model.name] = model
     })
 db.sequelize = sequelize
